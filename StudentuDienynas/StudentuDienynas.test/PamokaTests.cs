23a7ad07ef5879244235112662b1d1d3e9eb21df
Vilija <Simonaviciene@users.noreply.github.com>
2020-02-18 21:41:32 +0200

    pradinis

---
+++ StudentuDienynas/StudentuDienynas.test/PamokaTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StudentuDienynasLB;
+using System.Collections.Generic;
+
+namespace StudentuDienynas.test
+{
+    [TestClass]
+    public class PamokaTests
+    {
+        [TestMethod]
+        public void TurimeGrazintiTiekPazymiuKiekPridejome()
+        {
+            //Assign
+            Pamoka pamoka = new Pamoka(1, "Matematika");
+            //Act
+            pamoka.PridetiPazymi(1, 5);
+            List<int> pirmoTrimestroPazymiai = pamoka.GrazintiTrimestroPazymius(1);
+            //Assert
+            Assert.AreEqual(pirmoTrimestroPazymiai.Count, 1);
+            Assert.AreEqual(pirmoTrimestroPazymiai[0], 5);
+        }
+
+        [TestMethod]
+        public void PirmoTrimestroVidurkisTuriButi9()
+        {
+            //Assign
+            Pamoka pamoka = new Pamoka(1, "Matematika");
+            //Act
+            pamoka.PridetiPazymi(1, 8);
+            pamoka.PridetiPazymi(1, 9);
+            pamoka.PridetiPazymi(1, 10);
+            int pirmoTrimestroVidurkis = pamoka.PirmoTrimestroVidurkis();
+            //Assert
+            Assert.AreEqual(pirmoTrimestroVidurkis, 9);
+        }
+        [TestMethod]
+        public void NegalimaPridetiNeteisingoPazymio()
+        {
+            //Assign
+            Pamoka pamoka = new Pamoka(1, "Matematika");
+            //Act
+            pamoka.PridetiPazymi(1, -8);
+            int pirmoTrimestroVidurkis = pamoka.PirmoTrimestroVidurkis();
+            List<int> pirmoTrimestroPazymiai = pamoka.GrazintiTrimestroPazymius(1);
+            //Assert
+            Assert.AreEqual(pirmoTrimestroPazymiai.Count, 0);
+            Assert.AreEqual(pirmoTrimestroVidurkis, 0);
+        }
+        [TestMethod]
+        public void AntroTrimestroVidurkisTuriButi10()
+        {
+            //Assign
+            Pamoka pamoka = new Pamoka(1, "Matematika");
+            //Act
+            pamoka.PridetiPazymi(2, 10);
+            pamoka.PridetiPazymi(2, 10);
+            pamoka.PridetiPazymi(2, 10);
+            int antroTrimestroVidurkis = pamoka.AntroTrimestroVidurkis();
+            //Assert
+            Assert.AreEqual(antroTrimestroVidurkis, 10);
+        }
+    }
+}
---
+++ StudentuDienynas/StudentuDienynasLB/Pamoka.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentuDienynasLB
+{
+    public class Pamoka
+    {
+        public int ID { get; private set; }
+        public string Pavadinimas { get; private set; }
+        private List<int> pazymiai1Trimestras;
+        private List<int> pazymiai2Trimestras;
+        private List<int> pazymiai3Trimestras;
+
+        public Pamoka(int iD, string pavadinimas)
+        {
+            ID = iD;
+            Pavadinimas = pavadinimas;
+            this.pazymiai1Trimestras = new List<int>();
+            this.pazymiai2Trimestras = new List<int>();
+            this.pazymiai3Trimestras = new List<int>();
+        }
+        public int PirmoTrimestroVidurkis()
+        {
+            if (pazymiai1Trimestras.Count == 0) 
+            {
+                return 0;
+            }
+            int suma = pazymiai1Trimestras.Sum();
+            int vidurkis = suma / pazymiai1Trimestras.Count;
+
+            return vidurkis;
+
+        }
+        public int AntroTrimestroVidurkis()
+        {
+            if (pazymiai2Trimestras.Count == 0)
+            {
+                return 0;
+            }
+            int suma = pazymiai2Trimestras.Sum();
+            int vidurkis = suma / pazymiai2Trimestras.Count;
+
+            return vidurkis;
+        }
+        public int TrecioTrimestroVidurkis()
+        {
+            if (pazymiai3Trimestras.Count == 0)
+            {
+                return 0;
+            }
+            int suma = pazymiai3Trimestras.Sum();
+            int vidurkis = suma / pazymiai3Trimestras.Count;
+
+            return vidurkis;
+        }
+        public int MetinisVidurkis()
+        {
+            int metinisVidurkis = (PirmoTrimestroVidurkis() + AntroTrimestroVidurkis() + TrecioTrimestroVidurkis()) / 3;
+            return metinisVidurkis;
+        }
+        public void PridetiPazymi(int trimestroNr, int pazymys)
+        {
+            if(pazymys <= 0 || pazymys > 10)
+            {
+                return;
+            }
+
+            if (trimestroNr == 1)
+            {
+                pazymiai1Trimestras.Add(pazymys);
+            }
+            else if (trimestroNr == 2)
+            {
+                pazymiai2Trimestras.Add(pazymys);
+            }
+            else if (trimestroNr == 3)
+            {
+                pazymiai3Trimestras.Add(pazymys);
+            }
+            else
+            {
+                throw new System.ArgumentException(string.Format("Nebuna tokio trimestro {0}", trimestroNr));
+            }
+
+        }
+        public void IstrintiPazymi(int trimestroNr, int pazymioEilesNr)
+        {
+            if (trimestroNr == 1)
+            {
+                pazymiai1Trimestras.RemoveAt(pazymioEilesNr);
+            }
+            else if (trimestroNr == 2)
+            {
+                pazymiai2Trimestras.RemoveAt(pazymioEilesNr);
+            }
+            else if (trimestroNr == 3)
+            {
+                pazymiai3Trimestras.RemoveAt(pazymioEilesNr);
+            }
+            else
+            {
+                throw new System.ArgumentException(string.Format("Tokio pazymio eiles numerio {0} nera", pazymioEilesNr));
+            }
+
+        }
+        public List<int> GrazintiTrimestroPazymius(int trimestroNr)
+        {
+            if(trimestroNr == 1)
+            {
+                return pazymiai1Trimestras;
+            }
+            if (trimestroNr == 2)
+            {
+                return pazymiai2Trimestras;
+            }
+            if (trimestroNr == 3)
+            {
+                return pazymiai3Trimestras;
+            }
+            else
+            {
+                throw new System.ArgumentException(string.Format("Nebuna tokio trimestro {0}", trimestroNr));
+            }
+        }
+    }
+}
